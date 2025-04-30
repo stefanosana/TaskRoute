1ea37ba1fdac4152199cc9e87fa08bf444b9ab78
Stefano Sana <stefano.sana06@gmail.com>
2025-04-30 11:46:25 +0200

    aggiunto il ViewModels

--- Pages/AddTask.cshtml.cs
+++ Pages/AddTask.cshtml.cs
@@ -1,11 +1,14 @@
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using TaskRoute.Data;
 using TaskRoute.Models;
 
 namespace TaskRoute.Pages
 {
+    [Authorize] // Solo gli utenti autenticati possono accedere a questa pagina
     public class AddTaskModel : PageModel
     {
         private readonly ApplicationDbContext _context;
@@ -18,7 +21,7 @@ namespace TaskRoute.Pages
 
         public void OnGet()
         {
-            // niente da fare qui
+            // inizializza eventualmente valori di default
         }
 
         public async Task<IActionResult> OnPostAsync()
@@ -26,29 +29,57 @@ namespace TaskRoute.Pages
             //if (!ModelState.IsValid)
             //    return Page();
 
-            // Se ho dei dati di posizione:
-            var latStr = Request.Form["Latitude"];
-            if (!string.IsNullOrEmpty(latStr))
-            {
-                var loc = new Location
-                {
-                    Name = Request.Form["LocationName"],
-                    Address = Request.Form["LocationAddress"],
-                    City = Request.Form["City"],
-                    PostalCode = Request.Form["PostalCode"],
-                    Country = Request.Form["Country"],
-                    Latitude = double.Parse(latStr),
-                    Longitude = double.Parse(Request.Form["Longitude"])
-                };
-                _context.Locations.Add(loc);
-                await _context.SaveChangesAsync();
+            // Leggo i dati di Location dal form
+            var name = Request.Form["LocationName"].ToString().Trim();
+            var address = Request.Form["LocationAddress"].ToString().Trim();
+            var city = Request.Form["City"].ToString().Trim();
+            var postal = Request.Form["PostalCode"].ToString().Trim();
+            var country = Request.Form["Country"].ToString().Trim();
+            var latStr = Request.Form["Latitude"].ToString();
+            var lonStr = Request.Form["Longitude"].ToString();
 
+            if (!string.IsNullOrEmpty(latStr) && !string.IsNullOrEmpty(lonStr))
+            {
+                var lat = double.Parse(latStr);
+                var lon = double.Parse(lonStr);
+
+                // 1) cerco una Location con gli stessi dati (puoi adattare i criteri)
+                var loc = await _context.Locations
+                    .FirstOrDefaultAsync(l =>
+                        l.Name == name &&
+                        l.Address == address &&
+                        l.City == city &&
+                        l.PostalCode == postal &&
+                        l.Country == country &&
+                        l.Latitude == lat &&
+                        l.Longitude == lon
+                    );
+
+                // 2) se non esiste, la creo
+                if (loc == null)
+                {
+                    loc = new Location
+                    {
+                        Name = name,
+                        Address = address,
+                        City = city,
+                        PostalCode = postal,
+                        Country = country,
+                        Latitude = lat,
+                        Longitude = lon
+                    };
+                    _context.Locations.Add(loc);
+                    await _context.SaveChangesAsync(); // per ottenere loc.Id
+                }
+
+                // 3) assegno l'Id di Location alla Commission
                 Commission.LocationId = loc.Id;
             }
 
             // Imposto l'utente corrente
             Commission.UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
+            // 4) salvo la Commission
             _context.Commissions.Add(Commission);
             await _context.SaveChangesAsync();
 
---
+++ ViewModels/CommissionCreateViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskRoute.ViewModels
+{
+    // 1) Definisci un ViewModel per il form di creazione
+    public class CommissionCreateViewModel
+    {
+        // Proprietà Commission
+        [Required, StringLength(100)]
+        public string Title { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DueDate { get; set; }
+
+        // Proprietà Location
+        [Required, StringLength(100)]
+        public string LocationName { get; set; }
+
+        [StringLength(200)]
+        public string Address { get; set; }
+
+        [StringLength(50)]
+        public string City { get; set; }
+
+        [StringLength(20)]
+        public string PostalCode { get; set; }
+
+        [StringLength(50)]
+        public string Country { get; set; }
+
+        [Required]
+        public double Latitude { get; set; }
+
+        [Required]
+        public double Longitude { get; set; }
+    }
+
+}
